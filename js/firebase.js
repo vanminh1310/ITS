538864a2552848fb51f3665fec28fe6ec894d600
vanminh1310 <taminh39@gmail.com>
2020-08-12 15:42:22 +0700

    login firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,77 +1,27 @@
-// var config = {
-//     apiKey: "AIzaSyCQDOTIYf6_8FHSbxsckL80gtI8DQfCe6g",
-//     authDomain: "vvvvv-dc879.firebaseapp.com",
-//     databaseURL: "https://vvvvv-dc879.firebaseio.com",
-//     projectId: "vvvvv-dc879",
-//     storageBucket: "vvvvv-dc879.appspot.com",
-//     messagingSenderId: "740270975513",
-//     appId: "1:740270975513:web:f3ad584a819ce129f99888",
+var firebaseConfig = {
+    apiKey: "AIzaSyAe0_zKfODvMK8Otz8hz5LGKNayCECpiZw",
+    authDomain: "its-k59.firebaseapp.com",
+    databaseURL: "https://its-k59.firebaseio.com",
+    projectId: "its-k59",
+    storageBucket: "its-k59.appspot.com",
+    messagingSenderId: "552163072398",
+    appId: "1:552163072398:web:0deb2fc384dd6300a6ecdf",
+    measurementId: "G-YF04DFHNBT"
+  
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-// };
-// firebase.initializeApp(config);
 
-// firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     // User is signed in.
+  const auth = firebase.auth();
+  function  signIn(){
+    var gmail= document.getElementById("email_field");
+    var pass = document.getElementById("password_field");
+    const login =  auth.signInWithEmailAndPassword(gmail.value, pass.value);
 
-//     document.getElementById("user_div").style.display = "block";
-//     document.getElementById("login_div").style.display = "none";
-
-//     var user = firebase.auth().currentUser;
-
-//     if(user != null){
-
-//       var email_id = user.email;
-//       document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-
-//     }
-
-//   } else {
-//     // No user is signed in.
-
-//     document.getElementById("user_div").style.display = "none";
-//     document.getElementById("login_div").style.display = "block";
-   
-//   }
-// });
-
-// function login(){
-
-//   var userEmail = document.getElementById("email_field").value;
-//   var userPass = document.getElementById("password_field").value;
-
-//   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-
-//     window.alert("Error : " + errorMessage);
-
-//     // ...
-//   });
-
-// }
-
-// function logout(){
-//   firebase.auth().signOut();
-// }
-$(".div input").on("focus",function(){
-  $(this).addClass("focus");
-});
-
-$(".div input").on("blur",function(){
-  if($(this).val() == "")
-  $(this).removeClass("focus");
-});
-function check(form)/*function to check userid & password*/
-{
-/*the following code checkes whether the entered userid and password are matching*/
-if(form.userid.value == "admin" && form.pswrd.value == "admin")
-{
-  window.open('home.html','_self')/*opens the target page while Id & password matches*/
+    login.catch(e => alert(e.message));
+    
 }
-else
-{
- alert("Error Password or Usename")/*displays error message*/
-}
-}
+  
+
+
--- js/main.js
+++ js/main.js
@@ -17,3 +17,26 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+
+// $(".div input").on("focus",function(){
+// 	$(this).addClass("focus");
+//   });
+  
+//   $(".div input").on("blur",function(){
+// 	if($(this).val() == "")
+// 	$(this).removeClass("focus");
+//   });
+//   function check(form)/*function to check userid & password*/
+//   {
+//   /*the following code checkes whether the entered userid and password are matching*/
+//   if(form.userid.value == "admin" && form.pswrd.value == "admin")
+//   {
+// 	window.open('home.html','_self')/*opens the target page while Id & password matches*/
+//   }
+//   else
+//   {
+//    alert("Error Password or Usename")/*displays error message*/
+//   }
+//   }
+  
