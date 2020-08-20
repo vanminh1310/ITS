b14fd25e951c18bfeee2ae49b13d5ae80ef2b535
vanminh1310 <taminh39@gmail.com>
2020-08-20 08:30:07 +0700

    them modal

--- js/ota.js
+++ js/ota.js
@@ -18,3 +18,31 @@ red.on('value', function (red) //đọc dữ liệu tu firebase
     test.innerText = red.val();
     console.log(red.val())
     ;})
+
+    setInterval(displayclock,500);
+    function displayclock(){
+      var time = new Date();
+      var hrs = time.getHours();
+      var min = time.getMinutes();
+      var sec = time.getSeconds();
+  
+      if (hrs>12){
+        hrs = hrs -12;
+  
+      }
+      if(hrs == 12){
+        hrs = 12;
+      }
+      if(hrs<10){
+        hrs ='0'+hrs;
+      }
+      if(min<10){
+        min = '0' +min;
+  
+      }
+      if(sec<10){
+        sec= '0'+sec;
+      }
+      document.getElementById('clock').innerHTML=hrs + ':' + min +':' + sec;
+    }
+  
