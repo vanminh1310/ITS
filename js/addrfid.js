7cbdfe7c6bcd5532f94bc8182357adef17f96090
vanminh1310 <taminh39@gmail.com>
2020-08-30 21:40:12 +0700

    cập nhật lại hệ thống. Sửa addrfid, upotaota

--- js/addrfid.js
+++ js/addrfid.js
@@ -1,3 +1,33 @@
+// đồng hồ cho trang web 
+setInterval(displayclock,500);
+
+  function displayclock(){
+    var time = new Date();
+    var hrs = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+
+    if (hrs>12){
+      hrs = hrs -12;
+
+    }
+    if(hrs == 12){
+      hrs = 12;
+    }
+    if(hrs<10){
+      hrs ='0'+hrs;
+    }
+    if(min<10){
+      min = '0' +min;
+
+    }
+    if(sec<10){
+      sec= '0'+sec;
+    }
+    document.getElementById('clock').innerHTML=hrs + ':' + min +':' + sec;
+  }
+
+//kết nối  database 
 var config = {
     apiKey: "AIzaSyCQDOTIYf6_8FHSbxsckL80gtI8DQfCe6g",
     authDomain: "vvvvv-dc879.firebaseapp.com",
@@ -18,3 +48,29 @@ redrfid.on('value', function (redrfid) //đọc dữ liệu tu firebase
     rfid.innerText ="Mã RFID:" + redrfid.val();
     console.log(redrfid.val())
     ;})
+
+
+document.getElementById('btn_send').onclick = function()
+{
+  var inputid = document.getElementById('inputid').value;
+  var inputname = document.getElementById('inputname').value;
+  var inputsdt = document.getElementById('inputsdt').value;
+  var inputcd = document.getElementById('inputcd').value;
+// khai báo biến 
+if( inputsdt.length<1 && inputcd.length<1 && inputname.length<1 && inputid.length<1){
+  // kiểm tra xem có dữ liệu không 
+  alert("Unsuccessful");
+}
+ else{
+  //  có dữ liệu thì guử qua database
+  firebase.database().ref('ID/'+inputid).set({
+    Name: inputname,
+    Sdt: inputsdt,
+    Cd: inputcd
+
+
+  });
+  document.getElementById('form').reset();
+  alert("Success")
+}
+}
